1a86e9b20fb25eb868f2067e5cf75a1a516c5dcb
Eduardo Schmidt <eduardo@balena.io>
2021-10-12 18:03:53 -0300

    Implement basic storage feature

--- src/voicebot/features/storage.ts
+++ src/voicebot/features/storage.ts
@@ -1,6 +1,20 @@
-function run(data: string) {
-	console.log('Running storage feature..');
-	console.log(`Got ${data}`);
+import * as fs from 'fs';
+
+type MeetbotTranscriptionEvent = {
+	meet: string;
+	timestamp: string;
+	attendee: string;
+	text: string;
+};
+
+function run(data: MeetbotTranscriptionEvent) {
+	const filename = `meet-${data.meet}.log`;
+
+	fs.appendFile(filename, JSON.stringify(data) + '\n', function (err: any) {
+		if (err) {
+			throw err;
+		}
+	});
 }
 
 export { run };
