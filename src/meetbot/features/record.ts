657e3c120d5f65fc52e691599192495a06e6670a
Martin Rauscher <martin@balena.io>
2021-10-13 18:17:28 +0200

    finalize auto-recording feature
    Change-type: patch
    Signed-off-by: Martin Rauscher <martin@balena.io>

--- src/meetbot/features/record.ts
+++ src/meetbot/features/record.ts
@@ -12,20 +12,29 @@ export const attach = (bot: Bot) => {
 
 const startRecording = async (page: Page) => {
 	console.log('starting the recording...');
-	try {
-		await clickText(page, 'themes');
-		await page.waitForTimeout(1000);
+	await clickText(page, 'themes');
+	await page.waitForTimeout(2000);
 
-		await clickText(page, 'Recording');
-		await page.waitForTimeout(1000);
+	await clickText(page, 'Recording');
+	await page.waitForTimeout(2000);
 
-		await clickText(page, 'Start recording');
-		await page.waitForTimeout(1000);
+	const btn = await page.waitForXPath("//*[@aria-label='Start recording']");
+	await btn?.click();
+	await page.waitForTimeout(2000);
 
-		await clickText(page, 'Start');
-		await page.waitForTimeout(1000);
-	} finally {
-		// for debugging
-		// await page.screenshot({ path: 'after-recording-start.png' });
+	// HACK: this is so terrible...
+	const btns = await page.$$('.RveJvd.snByac');
+	for (const b of btns) {
+		if (await b.evaluate((n) => n.innerHTML === 'Start')) {
+			try {
+				await b.click();
+				break;
+			} catch {
+				// ...
+			}
+		}
 	}
+	await page.waitForTimeout(2000);
+	// for debugging
+	// await page.screenshot({ path: 'after-recording-start.png' });
 };
