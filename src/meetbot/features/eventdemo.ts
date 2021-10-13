b2f890858b321df9351dea81a23670dd498f387f
Martin Rauscher <martin@balena.io>
2021-10-13 12:30:42 +0200

    remove demo code from main bot code
    Change-type: patch
    Signed-off-by: Martin Rauscher <martin@balena.io>
    Co-authored-by: Vipul Gupta <vipulgupta2048@gmail.com>
    Signed-off-by: Martin Rauscher <martin@balena.io>

--- src/meetbot/features/eventdemo.ts
+++ src/meetbot/features/eventdemo.ts
@@ -2,12 +2,34 @@ import { Bot } from '..';
 
 export const attach = (bot: Bot) => {
 	console.log('Running eventdemo feature...');
+
 	bot.on('joined', ({ url }: { url: string }) => {
 		console.log('i joined a meeting!', url);
 	});
-	bot.on('captions', ({ url, text }: { url: string; text: string }) => {
-		console.log(`i got ${text} from ${url}`);
+
+	let sayHelloInProgress = false;
+	bot.on('captions', ({ url, texts }: { url: string; texts: string[] }) => {
+		if (!texts) {
+			return;
+		}
+		console.log(`i got ${texts.join(' ')} from ${url}`);
+
+		if (
+			texts.find((t) => /say[^a-z]*hello[^a-z]*jarvis/i.test(t)) &&
+			!sayHelloInProgress
+		) {
+			sayHelloInProgress = true;
+			bot.addJob(async (page) => {
+				await page.keyboard.type('What can I do for you, Sir?', {
+					delay: 10,
+				});
+				await page.keyboard.press('Enter');
+			});
+		} else {
+			sayHelloInProgress = false;
+		}
 	});
+
 	bot.on('left', ({ url }: { url: string }) => {
 		console.log('i left a meeting!', url);
 	});
