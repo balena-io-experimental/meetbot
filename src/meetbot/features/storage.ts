8889e74ba9b994469051b8af5380512477dc44a3
Martin Rauscher <martin@balena.io>
2021-10-13 12:31:18 +0200

    fix file-caption backup
    Change-type: patch
    Signed-off-by: Martin Rauscher <martin@balena.io>

--- src/meetbot/features/storage.ts
+++ src/meetbot/features/storage.ts
@@ -1,4 +1,4 @@
-import * as fs from 'fs';
+import { promises as fs } from 'fs';
 import { Bot } from '..';
 
 interface TranscriptionEvent {
@@ -15,16 +15,18 @@ interface JoinEvent {
 export const attach = (bot: Bot) => {
 	console.log('Running storage feature..');
 
+	let filename: string | null;
 	bot.on('joined', (joined: JoinEvent) => {
-		const filename = `meet-${joined.url}.log`;
+		const id = joined.url.split('/').pop();
+		filename = `meet-${id}.log`;
+	});
 
-		bot.on('data', (data: TranscriptionEvent) => {
-			console.log(`Got ${data}`);
-			fs.appendFile(filename, JSON.stringify(data) + '\n', function (err: any) {
-				if (err) {
-					throw err;
-				}
-			});
+	bot.on('captions', (data: TranscriptionEvent) => {
+		if (!filename) {
+			return;
+		}
+		bot.addJob(async () => {
+			await fs.appendFile(filename!, JSON.stringify(data) + '\n');
 		});
 	});
 };
