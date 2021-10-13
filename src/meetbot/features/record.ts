0f465d75be951e0890d4d57e2f85a601d1b862da
Martin Rauscher <martin@balena.io>
2021-10-13 12:31:38 +0200

    add auto-record feature
    Change-type: patch
    Signed-off-by: Martin Rauscher <martin@balena.io>

--- src/meetbot/features/record.ts
+++ src/meetbot/features/record.ts
@@ -1,10 +1,31 @@
+import { Page } from 'puppeteer-extra-plugin/dist/puppeteer';
 import { Bot } from '..';
+import { clickText } from '../pptr-helpers';
 
 export const attach = (bot: Bot) => {
-	console.log('Running recod feature..');
-	bot.on('settled', () => {
-		// This event happens when the bot is no longer clicking buttons and the UI is loaded
-		// The logic for this will press the record button
-		// bot.page.click('#record')
+	console.log('Running record feature..');
+	bot.on('joined', () => {
+		console.log('queuing: starting the recording...');
+		bot.addJob(startRecording);
 	});
+};
+
+const startRecording = async (page: Page) => {
+	console.log('starting the recording...');
+	try {
+		await clickText(page, 'themes');
+		await page.waitForTimeout(1000);
+
+		await clickText(page, 'Recording');
+		await page.waitForTimeout(1000);
+
+		await clickText(page, 'Start recording');
+		await page.waitForTimeout(1000);
+
+		await clickText(page, 'Start');
+		await page.waitForTimeout(1000);
+	} finally {
+		// for debugging
+		// await page.screenshot({ path: 'after-recording-start.png' });
+	}
 };
