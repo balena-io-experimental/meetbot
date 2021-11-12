3a0070d90596a5c88841a7f968185bef91102141
edufschmidt <eduardo@balena.io>
2021-11-12 11:02:21 -0300

    Update README and CHANGELOG

--- src/api.ts
+++ src/api.ts
@@ -6,21 +6,23 @@ const api = express.Router();
 api.use(express.json());
 
 api.post('/join', async (req, res) => {
-	let meetMetadata;
 	// Validate body
 	if (!req.body.url) {
-		return res.status(400).send('Missing `url` value in payload body.');
-	}
-	try {
-		meetMetadata = new URL(req.body.url);
-	} catch (e) {
-		return res.status(400).send('Invalid URL provided: ${url}');
+		return res
+			.status(400)
+			.send('Unable to join. Missing `url` value in payload body.');
 	}
 	// Try to spawn a meetbot for location
 	try {
-		await meetbotManager.spawnBot(meetMetadata.href);
+		await meetbotManager.spawnBot(req.body.url);
 	} catch (e: any) {
 		switch (e.message) {
+			case 'Invalid Google Meet URL.':
+				return res
+					.status(400)
+					.send(
+						`Unable to join. Invalid Google Meet URL provided: ${req.body.url}`,
+					);
 			case 'Maximum bot queue reached!':
 				return res.status(503).send('Max number of active meetbots reached');
 			case 'A bot is already in that location!':
@@ -31,7 +33,7 @@ api.post('/join', async (req, res) => {
 		}
 	}
 	// Meetbot is joining the meet soon
-	return res.status(202).send('A meetbot will be right there');
+	return res.status(202).send('A meetbot will be right there.');
 });
 
 api.get('/meets', async (_req, res) => {
@@ -48,7 +50,7 @@ api.post('/leave', async (req, res) => {
 	try {
 		meetMetadata = new URL(req.body.url);
 	} catch (e) {
-		return res.status(400).send('Invalid URL provided: ${url}');
+		return res.status(400).send(`Invalid URL provided: ${req.body.url}`);
 	}
 	// Try to kill meetbot for location
 	try {
@@ -56,14 +58,14 @@ api.post('/leave', async (req, res) => {
 	} catch (e: any) {
 		switch (e.message) {
 			case 'Could not find bot at specified location!':
-				return res.status(400).send('No meetbot found at that location');
+				return res.status(400).send('No meetbot found at that location.');
 			default:
 				console.error(e);
-				return res.status(500).send('Something unexpected happened');
+				return res.status(500).send('Something unexpected happened.');
 		}
 	}
 	// Meetbot will be leaving the meet soon
-	return res.status(202).send('Asking meetbot to leave');
+	return res.status(202).send('Asking meetbot to leave.');
 });
 
 export default api;
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import * as express from 'express';
-import path = require('path');
-import api from './api';
+import * as path from 'path';
 
 import * as meetbotManager from './meetbot-manager';
+import api from './api';
 
 const HTTP_PORT = process.env.HTTP_PORT || 8080;
 
 const server = express();
-server.use('/api', api);
+server.use(express.json());
 
+server.use('/api', api);
 server.use('/', express.static(path.join(__dirname, './www')));
 server.get('/*', (_req, res) =>
 	res.sendFile(path.resolve(__dirname, './www', 'index.html')),
