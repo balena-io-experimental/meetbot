631904039f13eb99dcada2144f1d2a32b43e1e3e
Vipul Gupta (@vipulgupta2048) <vipul@balena.io>
2021-12-05 02:39:41 +0530

    Add GREETING_MESSAGE configuration
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipul@balena.io>

--- src/meetbot/features/introduction.ts
+++ src/meetbot/features/introduction.ts
@@ -1,14 +1,16 @@
 import { Bot } from '..';
-import { postToChatJob } from '../pptr-helpers';
+import { postToChatJob } from '../meetbot-helpers';
+
+const GREETING_MESSAGE =
+	process.env.GREETING_MESSAGE ||
+	"Hello balenistas, it's your favorite bot, hubot!!";
 
 export const attach = (bot: Bot) => {
 	console.log('Introducing hubot in the meet - feature');
 
 	bot.on('joined', ({ meetURL }) => {
 		console.log('Joined the meeting: ', meetURL);
-		bot.addJob(
-			postToChatJob("Hello balenistas, it's your favorite bot, hubot!!"),
-		);
+		bot.addJob(postToChatJob(GREETING_MESSAGE));
 	});
 
 	bot.on('left', ({ meetURL }) => {
